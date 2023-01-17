1e65661ecda588a52c511cf6d9b0fee53bb04a58
Murendeni Nevondo <mnevondo727@gmail.com>
2023-01-17 12:02:57 +0200

    Add project files.

---
+++ DayOne/Program.cs
@@ -0,0 +1,99 @@
+﻿namespace DayOne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string filePath = @"C:\2022 projects\DotNet\AdventOfCode_2022\DayOne\Inputs\input.txt";
+
+            //Part one output
+            DayOnePartOne(filePath);
+
+            //Part two output
+            DayOnePartTwo(filePath);
+        }
+
+        static void DayOnePartOne(string fileInputPath)
+        {
+            try
+            {
+                var caloriesInput = File.ReadAllLines(fileInputPath);
+
+                int currentMaxCalorie = default;
+                int elfCalorie = default;
+
+                foreach (string calorie in caloriesInput)
+                {
+                    if (calorie == string.Empty)
+                    {
+                        if (currentMaxCalorie > elfCalorie)
+                            elfCalorie = currentMaxCalorie;
+                        currentMaxCalorie = 0; //reset max
+                    }
+                    else
+                    {
+                        currentMaxCalorie += Convert.ToInt32(calorie);
+                    }
+
+                }
+
+                Console.WriteLine(elfCalorie);
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static void DayOnePartTwo(string fileInputPath)
+        {
+            try
+            {
+                var caloriesInput = File.ReadAllLines(fileInputPath);
+
+                var topThreeCalories = new int[caloriesInput.Length];
+
+                int currentMaxCalorie = default;
+                int sum = default;
+
+                foreach (string calorie in caloriesInput)
+                {
+                    if (calorie == string.Empty)
+                    {
+                        for (int i = 2; i >= 0; i--)
+                        {
+                            if (currentMaxCalorie > topThreeCalories[i])
+                            {
+                                if (i != 2)
+                                {
+                                    topThreeCalories[i + 1] = topThreeCalories[i];
+                                }
+                                topThreeCalories[i] = currentMaxCalorie;
+                            }
+                        }
+
+                        //Reset max and move to the next ELF
+                        currentMaxCalorie = 0;
+                    }
+                    else
+                    {
+                        currentMaxCalorie += Convert.ToInt32(calorie);
+                    }
+                }
+
+                foreach (int maxCalorie in topThreeCalories)
+                {
+                    sum += maxCalorie;
+                }
+                Console.WriteLine(sum);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ DayTwo/Program.cs
@@ -0,0 +1,104 @@
+﻿namespace DayTwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                const string filePath = @"C:\2022 projects\DotNet\AdventOfCode_2022\DayTwo\Inputs\input.txt";
+
+                var rockPaperScissorsInput = File.ReadAllLines(filePath);
+
+                int totalScore = 0;
+
+                foreach (string line in rockPaperScissorsInput)
+                {
+                    string myShape = MapInputCharToShape(line[2]);
+                    string opponentShape = MapInputCharToShape(line[0]);
+
+                    int roundScore = ScoreRound(myShape, opponentShape);
+                    totalScore += roundScore;
+                    //Console.WriteLine($"My Shape: {myShape} Opponent Shape: {opponentShape} = {roundScore}");
+
+                }
+                Console.WriteLine($"My Total Score: {totalScore}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static string MapInputCharToShape(char character)
+        {
+            switch (character)
+            {
+                case 'A' or 'X':
+                    return "Rock";
+                case 'Y' or 'B':
+                    return "Paper";
+                case 'Z' or 'C':
+                    return "Scissors";
+                default:
+                    return string.Empty;
+            }
+        }
+
+        static int MapShapeToScore(string shape)
+        {
+            switch (shape)
+            {
+                case "Rock":
+                    return 1;
+                case "Paper":
+                    return 2;
+                case "Scissors":
+                    return 3;
+                default:
+                    return 0;
+            }
+        }
+
+        static bool Won(string myShape, string opponentShape)
+        {
+            if (myShape.Equals("Rock") && opponentShape.Equals("Scissors"))
+            {
+                return true;
+            }
+            else if (myShape.Equals("Scissors") && opponentShape.Equals("Paper"))
+            {
+                return true;
+            }
+            else if (myShape.Equals("Paper") && opponentShape.Equals("Rock"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        static bool IsADraw(string myShape, string opponentShape)
+        {
+            return myShape == opponentShape;
+        }
+
+        static int ScoreRound(string myShape, string opponentShape)
+        {
+            int points = MapShapeToScore(myShape);
+
+            if (Won(myShape, opponentShape))
+            {
+                points += 6;
+            }
+            else if (IsADraw(myShape, opponentShape))
+            {
+                points += 3;
+            }
+
+            return points;
+        }
+    }
+}
