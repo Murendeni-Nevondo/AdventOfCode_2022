3caf9576eca38194a0bfa6c42f141429224087ec
Murendeni Nevondo <mnevondo727@gmail.com>
2023-01-21 11:41:16 +0200

    Day Three Part Two Solution

--- DayThree/Program.cs
+++ DayThree/Program.cs
@@ -13,6 +13,7 @@
 
                 var input = File.ReadAllLines(filePath);
 
+                //Part One
                 foreach (var line in input)
                 {
                     try
@@ -31,7 +32,78 @@
                     catch (Exception)
                     {}
                 }
-                Console.WriteLine(total);
+                Console.WriteLine($"Part One: {total}");
+
+                int counter = 0;
+
+                var ruckSackGroupList = new string[3];
+
+                //Part Two
+                var partTwoTotal = 0;
+
+                foreach(var item in input)
+                {
+                    ruckSackGroupList[counter] = item;
+                    counter++;
+
+                    if (counter % 3 is 0)
+                    {
+                        var ruckSackOne = new List<char>();
+                        var ruckSackTwo = new List<char>();
+                        var ruckSackThree = new List<char>();
+
+                        for (int j = 0; j < 3; j++)
+                        {
+                            if (j == 0)
+                            {
+                                foreach (var d in ruckSackGroupList[0].ToArray())
+                                {
+                                    ruckSackOne.Add(d);
+                                }
+                            }
+
+                            if (j == 1)
+                            {
+                                foreach (var e in ruckSackGroupList[1].ToArray())
+                                {
+                                    ruckSackTwo.Add(e);
+                                }
+                            }
+
+                            if (j == 2)
+                            {
+                                foreach (var f in ruckSackGroupList[2].ToArray())
+                                {
+                                    ruckSackThree.Add(f);
+                                }
+                            } 
+                        }
+                        
+                        var commonItemInGroup = ruckSackOne.Intersect(ruckSackTwo).Intersect(ruckSackThree).ToList().FirstOrDefault();
+
+                        try
+                        {
+                            partTwoTotal += itemPriorityDictionary.lowarCaseLetterValuePairs[commonItemInGroup];
+                        }
+                        catch (Exception)
+                        {
+
+                        }
+
+                        try
+                        {
+                            partTwoTotal += itemPriorityDictionary.upperCaseLetterValuePairs[commonItemInGroup];
+                        }
+                        catch (Exception)
+                        {
+
+                        }
+
+                        ruckSackGroupList = new string[3];
+                        counter = 0;
+                    }
+                }
+                Console.WriteLine($"Part Two: { partTwoTotal }");
             }
             catch (Exception ex)
             {
